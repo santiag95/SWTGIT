aadb1837f5eb85206a638c0921b7032a76afcbef
santiag95 <santiag95@hotmail.com>
2017-05-24 10:51:06 +0200

    Added Javadoc comment of one of the  methods in TrianglePictureFilter class

--- iMage.geometrify/src/main/java/org/iMage/geometrify/TrianglePictureFilter.java
+++ iMage.geometrify/src/main/java/org/iMage/geometrify/TrianglePictureFilter.java
@@ -2,8 +2,20 @@ package org.iMage.geometrify;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-
+/**
+ * The Filter that edits the given photo with the primitive figures.
+ *
+ * @author Santiago Tafur
+ * @version 1.0
+ */
 public class TrianglePictureFilter extends AbstractPrimitivePictureFilter {
+	
+	/**
+	 * Constructs the triangle filter based on where the pointGenerator was placed.
+	 *
+	 * @param pointGenerator
+	 *            the generator of the point
+	 */
 	public TrianglePictureFilter(IPointGenerator pointGenerator) {
 		super(pointGenerator);
 	}
