834baa7f71208aae16bf03f08d2aa2c54b74019c
santiag95 <santiag95@hotmail.com>
2017-05-24 10:49:22 +0200

    Finished and added the JavaDoc of the RandomPointGeneratorClass

--- iMage.geometrify/src/main/java/org/iMage/geometrify/RandomPointGenerator.java
+++ iMage.geometrify/src/main/java/org/iMage/geometrify/RandomPointGenerator.java
@@ -1,7 +1,7 @@
 package org.iMage.geometrify;
 
 import java.awt.Point;
-
+import java.util.Random;
 /**
  * Provides an infinite source of points at random coordinates within a given
  * range.
@@ -12,6 +12,15 @@ import java.awt.Point;
 public class RandomPointGenerator implements IPointGenerator {
 
 	/**
+	 * The maximum X coordinate for the point thats going to be created.
+	 */
+	private int maxX;
+	/**
+	 * The maximum Y coordinate for the point thats going to be created.
+	 */
+	private int maxY;
+	
+	/**
 	 * Constructs the generator for points within the specified coordinate
 	 * space.
 	 *
@@ -21,14 +30,20 @@ public class RandomPointGenerator implements IPointGenerator {
 	 *            the maximum y coordinate
 	 */
 	public RandomPointGenerator(int width, int height) {
-		/*
-		 * YOUR SOLUTION HERE
-		 */
+		maxX = width;
+		maxY = height;
 	}
-
-	@Override
+	
+	
+	/**
+	 * Generates a new random point given the coordinates in the constructor.
+	 * @return Point the Point generated within the random coordinates.
+	 */
 	public Point nextPoint() {
+		Random rand = new Random();
+		int randomX = rand.nextInt(maxX);
+		int randomY = rand.nextInt(maxY); 
 		
-		return null;
+		return new Point(randomX, randomY);
 	}
 }
