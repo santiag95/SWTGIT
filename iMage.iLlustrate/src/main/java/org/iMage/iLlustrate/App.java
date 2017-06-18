a7770e97b052cd463e9c7d35718cd8f1b621c867
santiag95 <santiag95@hotmail.com>
2017-06-18 17:02:39 +0200

    Added the new module iLlustrate

---
+++ iMage.iLlustrate/src/main/java/org/iMage/iLlustrate/App.java
@@ -0,0 +1,13 @@
+package org.iMage.iLlustrate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
